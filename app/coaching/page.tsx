e698c3516dae2202c10ba448d3df716dfaf13a20
VivienIngrams <vivingrams@gmail.com>
2023-11-20 17:07:27 +0000

    Design Coaching page

--- app/coaching/page.tsx
+++ app/coaching/page.tsx
@@ -1,27 +1,34 @@
 import React from "react";
 import Image from "next/image";
+import Link
+ from "next/link";
 
 const Coaching = () => {
   return (
     <section id="Coaching">
-       <div className="relative">
-   
+
+    <div className="">
+      {/* Unsplash image */}
       <div className="w-full" style={{ backgroundPosition: 'center bottom' }}>
         <Image
-          className="absolute -top-35 left-0 right-0 bottom-0 min-h-screen bg-fixed bg-center bg-cover h-full w-full "
+          className="absolute top-0 left-0   bg-fixed bg-center bg-cover h-1/2 w-full "
           src="https://images.unsplash.com/uploads/14122810486321888a497/1b0cc699?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29hY2hpbmd8ZW58MHwwfDB8fHww"
-          height={500}
-          width={600}
-          alt="Coaching"
+          width={1400}
+          height={700}
+          alt="background image"
         />
         <div className="absolute top-0 left-0 w-full min-h-screen bg-cover bg-[rgb(179,217,255,0.6)]" />
-<div className="h-screen w-full"></div>
 
-        
+        {/* Ooverlay */}
+        <div className="relative top-0 left-0 w-full min-h-screen flex items-center justify-center">
+          <div className="-translate-y-1/3">
+            <h1>Coaching</h1>
+          </div>
+        </div>
       </div >
     </div>
-     
-    </section>
+  </section>
+
   );
 };
 
